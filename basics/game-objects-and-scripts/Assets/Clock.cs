fadb2b7e91a91e635580e127d981c5f0fde2bb89
exlef <50216668+exlef@users.noreply.github.com>
2024-11-15 15:58:08 +0300

    creating hour minutes seconds arms

--- basics/game-objects-and-scripts/Assets/Clock.cs
+++ basics/game-objects-and-scripts/Assets/Clock.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    [SerializeField] GameObject hourIndicator;
-    [SerializeField] Transform hourIndicatorParent;
+    [SerializeField] GameObject hourIndicator, hourIndicatorParent;
     [SerializeField] float radius = 4;
+	[SerializeField] Transform hoursPivot, minutesPivot, secondsPivot;
     
     void Start()
     {
         for (int i = 0; i < 12; i++)
         {
-            var go = Instantiate(hourIndicator, new Vector3(0, 0, -0.25f), Quaternion.identity, hourIndicatorParent);
+            var go = Instantiate(hourIndicator, new Vector3(0, 0, -0.25f), Quaternion.identity, hourIndicatorParent.transform);
             float angle = (360/12) * i;
             
             go.transform.Rotate(Vector3.forward, angle);
@@ -19,5 +19,3 @@ public class Clock : MonoBehaviour
         }
     }
 }
-
-//  .NET 8.0.11~arm64 executable path: /Users/amedduman/Library/Application Support/Code/User/globalStorage/ms-dotnettools.vscode-dotnet-runtime/.dotnet/8.0.11~arm64/dotnet
