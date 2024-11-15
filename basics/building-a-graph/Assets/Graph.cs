822e6c0348251f5778400f03c0239ec0a03fe93f
exlef <50216668+exlef@users.noreply.github.com>
2024-11-15 23:13:43 +0300

    improvements

--- basics/building-a-graph/Assets/Graph.cs
+++ basics/building-a-graph/Assets/Graph.cs
@@ -7,14 +7,18 @@ public class Graph : MonoBehaviour
 
     void Start()
     {
-        float range = 2;
-        float factor = cubeCount / range;
+        var range = 2;
+        var factor = cubeCount / range;
+        var scale = Vector3.one / factor;
+        var position = Vector3.zero;
+        
         for (int i = 0; i < cubeCount; i++)
         {
             var go = Instantiate(pointPrefab, transform);
 
-            go.localScale = Vector3.one / factor;
-            go.localPosition = new Vector3(i / factor - 1, 0, 0) + new Vector3(go.localScale.x/2, 0, 0);
+            go.localScale = scale;
+            position.x = i / factor - 1 + go.localScale.x / 2;
+            go.localPosition = position;
         }
     }
 }
