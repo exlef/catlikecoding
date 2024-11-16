d79c10835795a959f32f91644a13a253b6647a31
exlef <50216668+exlef@users.noreply.github.com>
2024-11-16 14:25:24 +0300

    animate graph

--- basics/building-a-graph/Assets/Graph.cs
+++ basics/building-a-graph/Assets/Graph.cs
@@ -5,21 +5,40 @@ public class Graph : MonoBehaviour
     [SerializeField] Transform pointPrefab;
     [SerializeField, Min(10)] int resolution = 10;
 
+    Transform[] points;
+
     void Start()
     {
+        points = new Transform[resolution];
+
         var range = 2;
         var factor = (float)resolution / range;
         var scale = Vector3.one / factor;
+        scale.y = 1;
         var position = Vector3.zero;
 
         for (int i = 0; i < resolution; i++)
         {
             var go = Instantiate(pointPrefab, transform);
-
+            
             go.localScale = scale;
             position.x = i / factor - 1 + go.localScale.x / 2;
-            position.y = position.x * position.x * position.x;
             go.localPosition = position;
+
+            points[i] = go;
         }
     }
+
+    void Update () 
+    {
+        var time = Time.time;
+		for (int i = 0; i < points.Length; i++)
+        {
+            var point = points[i];
+            var position = point.transform.localPosition;
+            
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+	}
 }
