32a0ae212e7854c93e462336094880a78675a2f5
exlef <50216668+exlef@users.noreply.github.com>
2024-11-16 10:17:45 +0300

    color points in graph based on color

--- basics/building-a-graph/Assets/Graph.cs
+++ basics/building-a-graph/Assets/Graph.cs
@@ -3,21 +3,22 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     [SerializeField] Transform pointPrefab;
-    [SerializeField] float cubeCount = 10;
+    [SerializeField, Min(10)] int resolution = 10;
 
     void Start()
     {
         var range = 2;
-        var factor = cubeCount / range;
+        var factor = (float)resolution / range;
         var scale = Vector3.one / factor;
         var position = Vector3.zero;
-        
-        for (int i = 0; i < cubeCount; i++)
+
+        for (int i = 0; i < resolution; i++)
         {
             var go = Instantiate(pointPrefab, transform);
 
             go.localScale = scale;
             position.x = i / factor - 1 + go.localScale.x / 2;
+            position.y = position.x * position.x * position.x;
             go.localPosition = position;
         }
     }
