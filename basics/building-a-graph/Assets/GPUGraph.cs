beae960ea88f5c0ba779f5b40f7cdb948ad8206e
exlef <50216668+exlef@users.noreply.github.com>
2024-11-22 21:56:31 +0300

    add other functions for graph

--- basics/building-a-graph/Assets/GPUGraph.cs
+++ basics/building-a-graph/Assets/GPUGraph.cs
@@ -5,6 +5,7 @@ public class GPUGraph : MonoBehaviour
     [SerializeField] ComputeShader computeShader;
     const int maxResolution = 1000;
     [SerializeField, Range(10, maxResolution)] int resolution = 10;
+    [SerializeField] FunctionLibrary.FunctionName function;
     [SerializeField] Material material;
 	[SerializeField] Mesh mesh;
     ComputeBuffer positionsBuffer;
@@ -26,13 +27,14 @@ public class GPUGraph : MonoBehaviour
 
     void UpdateFunctionOnGPU ()
     {
+        var kernelIndex = (int)function;
 		float step = 2f / resolution;
 		computeShader.SetInt("_Resolution", resolution);
 		computeShader.SetFloat("_Step", step);
 		computeShader.SetFloat("_Time", Time.time);
-        computeShader.SetBuffer(0, "_Positions", positionsBuffer); // Its first argument is the index of the kernel function, because a compute shader can contain multiple kernels and buffers can be linked to specific ones. We could get the kernel index by invoking FindKernel on the compute shader, but our single kernel always has index zero so we can use that value directly.
+        computeShader.SetBuffer(kernelIndex, "_Positions", positionsBuffer); // Its first argument is the index of the kernel function, because a compute shader can contain multiple kernels and buffers can be linked to specific ones. We could get the kernel index by invoking FindKernel on the compute shader, but our single kernel always has index zero so we can use that value directly.
         int groups = Mathf.CeilToInt(resolution / 8f);
-		computeShader.Dispatch(0, groups, groups, 1);
+		computeShader.Dispatch(kernelIndex, groups, groups, 1);
 
         material.SetBuffer("_Positions", positionsBuffer);
 		material.SetFloat("_Step", step);
