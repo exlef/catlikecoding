109badd79ea44ffba082c8d3f30445eeeb72955f
exlef <50216668+exlef@users.noreply.github.com>
2024-11-24 14:51:23 +0300

    first iteration for fractal

--- basics/building-a-graph/Assets/Fractal.cs
+++ basics/building-a-graph/Assets/Fractal.cs
@@ -10,18 +10,30 @@ public class Fractal : MonoBehaviour
 
         if(depth <= 1) return;
 
-        var child1 = CreateChild(Vector3.right);
-        var child2 = CreateChild(Vector3.up);
+        Fractal childA = CreateChild(Vector3.up, Quaternion.identity);
+        Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0, 0, -90));
+        Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+        Fractal childD = CreateChild(Vector3.forward, Quaternion.Euler(90f, 0f, 0f));
+		Fractal childE = CreateChild(Vector3.back, Quaternion.Euler(-90f, 0f, 0f));
 
-        child1.transform.SetParent(transform, false);
-        child2.transform.SetParent(transform, false);
+        childA.transform.SetParent(transform, false);
+        childB.transform.SetParent(transform, false);
+        childC.transform.SetParent(transform, false);
+		childD.transform.SetParent(transform, false);
+		childE.transform.SetParent(transform, false);
     }
 
-    Fractal CreateChild(Vector3 direction)
+    void Update ()
+    {
+		transform.Rotate(0f, 22.5f * Time.deltaTime, 0f);
+	}
+
+    Fractal CreateChild(Vector3 direction, Quaternion rotation)
     {
         var child = Instantiate(this);
         child.depth = depth - 1;
         child.transform.localPosition = 0.75f * direction;
+        child.transform.localRotation = rotation;
         child.transform.localScale = 0.5f * Vector3.one; 
         return child;
     }
