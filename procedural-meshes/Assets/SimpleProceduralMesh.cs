a623b2fc2543f15f0af2a1365e21a06f1e55d962
exlef <50216668+exlef@users.noreply.github.com>
2024-12-08 14:08:42 +0300

    generate quad simple api

--- procedural-meshes/Assets/SimpleProceduralMesh.cs
+++ procedural-meshes/Assets/SimpleProceduralMesh.cs
@@ -11,25 +11,26 @@ public class SimpleProceduralMesh : MonoBehaviour
         };
 
         mesh.vertices = new Vector3[] {
-            Vector3.zero, Vector3.right, Vector3.up
+            Vector3.zero, Vector3.right, Vector3.up, new Vector3(1f, 1f),
         };
 
         mesh.normals = new Vector3[] {
-            Vector3.back, Vector3.back, Vector3.back
+            Vector3.back, Vector3.back, Vector3.back, Vector3.back
         };
 
         mesh.uv = new Vector2[] {
-            Vector2.zero, Vector2.right, Vector2.up
+            Vector2.zero, Vector2.right, Vector2.up, Vector2.one
         };
 
         mesh.tangents = new Vector4[] {
+            new(1f, 0f, 0f, -1f),
             new(1f, 0f, 0f, -1f),
             new(1f, 0f, 0f, -1f),
             new(1f, 0f, 0f, -1f)
         };
 
         mesh.triangles = new int[] {
-            0, 2, 1
+            0, 2, 1, 1, 2, 3
         };
 
         GetComponent<MeshFilter>().mesh = mesh;
