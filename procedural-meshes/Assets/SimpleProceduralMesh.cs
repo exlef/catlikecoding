1136e77bb9de94e081687149a9877b1e6319c029
exlef <50216668+exlef@users.noreply.github.com>
2024-12-08 13:59:00 +0300

    generate triangle mesh

---
+++ procedural-meshes/Assets/SimpleProceduralMesh.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SimpleProceduralMesh : MonoBehaviour
+{
+    void OnEnable()
+    {
+        var mesh = new Mesh
+        {
+            name = "Procedural Mesh"
+        };
+
+        mesh.vertices = new Vector3[] {
+            Vector3.zero, Vector3.right, Vector3.up
+        };
+
+        mesh.normals = new Vector3[] {
+            Vector3.back, Vector3.back, Vector3.back
+        };
+
+        mesh.uv = new Vector2[] {
+            Vector2.zero, Vector2.right, Vector2.up
+        };
+
+        mesh.tangents = new Vector4[] {
+            new(1f, 0f, 0f, -1f),
+            new(1f, 0f, 0f, -1f),
+            new(1f, 0f, 0f, -1f)
+        };
+
+        mesh.triangles = new int[] {
+            0, 2, 1
+        };
+
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+}
